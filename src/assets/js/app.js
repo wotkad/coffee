06a9a21e487ff2e3dfa1957aba610b9508ba6d93
wotkad <wotkad@gmail.com>
2022-07-20 17:53:05 +0300

    first commit

---
+++ src/assets/js/app.js
@@ -0,0 +1,6 @@
+require("./preloader/preloader");
+require("./linksChecker/checkInternalLinks/checkInternalLinks");
+require("./routing/routing");
+require("./scroll/smoothScroll/smoothScroll");
+require("./scroll/scrollReveal/scrollReveal");
+require("./linksChecker/checkTarget/checkTarget");

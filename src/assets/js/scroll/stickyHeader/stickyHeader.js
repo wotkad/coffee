8fc75665f0fb852391aa6024b124ff2b6d004aec
wotkad <wotkad@gmail.com>
2022-09-12 01:32:46 +0300

    fixes

--- src/assets/js/scroll/stickyHeader/stickyHeader.js
+++ src/assets/js/scroll/stickyHeader/stickyHeader.js
@@ -2,16 +2,20 @@ import $ from "jquery";
 
 export default function stickyHeader() {
   let header = $('.header-mob');
+  let contentHeight = $('.card-popup__content');
   let prevScrollpos = window.pageYOffset;
   window.onscroll = function() {
     let currentScrollPos = window.pageYOffset;
     if (prevScrollpos > currentScrollPos) {
       header.removeClass('active');
+      contentHeight.removeClass('active');
     } else {
       header.addClass('active');
+      contentHeight.addClass('active');
     }
     if (window.pageYOffset <= 0) {
       header.removeClass('active');
+      contentHeight.removeClass('active');
     }
     prevScrollpos = currentScrollPos;
   }
